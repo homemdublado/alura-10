344be33d6fbac680401d43ada506eccdd80d0b7b
homemdublado <00001101844851SP@al.educacao.sp.gov.br>
2024-11-26 13:14:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Brasileirão',
+    'Qual foi o time a ganhar o priemiro Brasileirão?',
+    'Bahia'
+)
+
+criaCartao(
+    'Premier League',
+    'Qual o maior campeão da Premier League?',
+    'Manchester United'
+)
+
+criaCartao(
+    'LaLiga',
+    'Qual é o maior atilheiro da LaLiga?',
+    'Lionel Messi'
+)
+
+criaCartao(
+    'Copa do Mundo',
+    'Qual é a seleção que mais ganhou a Copa do Mundo?',
+    'Brasil'
+)
